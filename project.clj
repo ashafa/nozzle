204b6001e90bfd381a7afe2ba515a0b8529fa36b
Tunde Ashafa <tunde.ashafa@gmail.com>
2010-02-22 18:18:38 -0500

    Readme update. More cleaning up.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.ashafa/nozzle "1.0.0-SNAPSHOT" 
-  :description "A Clojure library to connect to twitters streaming API." 
+  :description "A Clojure library for streaming stauses from the Twitter Streaming API." 
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[autodoc "0.7.0"]]
